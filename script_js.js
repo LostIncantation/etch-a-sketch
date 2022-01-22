77d581c1ff950955426c677b2a4cd8ea4aff2e06
Heath <hhcowled@gmail.com>
2022-01-22 11:29:09 +1100

    Add dynamically populated grid
    Script now has a function to dynamically build grid with divs which can
    then be highlighted on mouseover event

--- script_js.js
+++ script_js.js
@@ -1,11 +1,47 @@
+const etchContainer = document.querySelector(".etch-grid-container")
+
+/*
+    Fills a grid container with the relevant number of pixels
+*/
+function fillGridContainer(gridPixels){
+    console.log(gridPixels);
+    etchContainer.style.gridTemplate = `repeat(${gridPixels}, 1fr) / repeat(${gridPixels}, 1fr)`;
+    console.log("Ran");
+
+    const fillerSpan = document.querySelector("#filler");
+    const gridPixel = document.createElement("DIV");
+    gridPixel.className = 'grid-pixel';
+    let i = 0;
+    let gridPixelsArray = [];
+
+    while(i<gridPixels*gridPixels){
+        let newGridPixel = document.createElement("DIV");
+        newGridPixel.className = "grid-pixel";
+        newGridPixel.id = `grid-pixel-${i}`;
+        gridPixelsArray.push(newGridPixel);
+        i++
+    }
+
+
+    let j = 0;
+    while (j<gridPixelsArray.length){
+        etchContainer.appendChild(gridPixelsArray[j]);
+        console.log('Appending div');
+        j++;
+    }
+
+    addEtchColouring();
+}
 
 
 /*
-    Targets every element of code with "grid-item" and adds an event listener
+    Targets every element of code with ".grid-pixel" and adds an event listener
     which activates highlightGrid()
 */
-let etchTarget = document.querySelectorAll(".grid-item");
-etchTarget.forEach(element => element.addEventListener("mouseover", highlightGrid));
+function addEtchColouring(){
+    const etchTarget = document.querySelectorAll(".grid-pixel");
+    etchTarget.forEach(element => element.addEventListener("mouseover", highlightGrid));
+}
 
 /*
     Sets the background colour of the object upon which the function is called
@@ -13,3 +49,5 @@ etchTarget.forEach(element => element.addEventListener("mouseover", highlightGri
 function highlightGrid(){
     this.style.backgroundColor = "gold";
 }
+
+fillGridContainer(16);
